f06b3c52276e04962df2622607570da631aead9b
emihaba <emilapc2@gmail.com>
2017-06-08 18:15:45 +0200

    Added induction field to Field

--- Assets/Scripts/Field.cs
+++ Assets/Scripts/Field.cs
@@ -4,13 +4,5 @@ using UnityEngine;
 
 public class Field : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static Vector3 induction = new Vector3(0.0f, 1.0f, 0.0f);
 }
